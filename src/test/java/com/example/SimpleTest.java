e93ec45cc53731f6dd92989f45c5b937f6a062bb
Nikita Sukhodoev <dogmach94@yandex.ru>
2022-03-03 20:09:00 +0400

    first commit

---
+++ src/test/java/com/example/SimpleTest.java
@@ -0,0 +1,18 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class SimpleTest {
+    @Test
+    void assertTrueTest(){
+        assertTrue(999 > 111);
+    }
+
+    @Test
+    void assertFalseTest(){
+        assertFalse(1000 > 5000);
+    }
+}
